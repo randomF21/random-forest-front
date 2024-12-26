7e4544dd22adf5d0969aa3a39187590d08ea49b5
Linda <147208499+karther13@users.noreply.github.com>
2024-12-26 16:19:49 -0500

    se modificó el diseño del login - se realizó el diseño del registro

--- src/rutas/rutas.jsx
+++ src/rutas/rutas.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 // pagina del sistema
+import LandingPage from '../pages/LandingPage';
 import LoginPage from '../pages/LoginPage';
+import RegistroPage from '../pages/RegistroPage';
 import DashboardPage from '../pages/DashboardPage';
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LoginPage />} />
+                <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/registro" element={<RegistroPage />} />
                 <Route path="/dashboard" element={<DashboardPage />} />
             </Routes>
             <Toaster position="top-right" reverseOrder={false} />
