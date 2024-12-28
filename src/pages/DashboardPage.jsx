17f1a7797c499c38715a3d43da0728f9e5d21d53
Oftal210 <redsunestudio@gmail.com>
2024-12-28 16:18:33 -0500

    agrego funcionalidad en el menu de navegacion y las opciones faltantes, suo de datos para el menu de navegación

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -3,10 +3,14 @@ import Navbar from '../componentes/navegacion/navbar';
 
 
 const DashboardPage = () => {
+
+    const user = JSON.parse(sessionStorage.getItem('usuario')); // Suponiendo que guardas un objeto con los datos del usuario 
+    const { rol, nombre, apellido } = user;
+
     return (
         <>
             <div className="flex m-0 p-0">
-                <SideBar dato="https://picsum.photos/200" />
+                <SideBar ruta_foto="https://picsum.photos/200" nombreUsuario={nombre+apellido} rol={rol} />
                 <div className='w-full'>
                     <Navbar titulo={'Bienvenid@'} />
                     <div className="ml-60 mt-40 bg-white h-screen p-8">
